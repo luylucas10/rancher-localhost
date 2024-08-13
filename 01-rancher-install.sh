47b02ea4123b076763fa1e6ebb513717ffe41cfd
luylucas10 <luylucas10@gmail.com>
2024-08-12 21:52:35 -0300

    part-2

--- 01-rancher-install.sh
+++ 01-rancher-install.sh
@@ -1,3 +1,7 @@
+#rancher 123456
+#su - 123456 
+
+# for every node
 # preparing ubuntu-vm for rke2 installation
 systemctl stop ufw # stop the software firewall
 systemctl disable ufw # disable the software firewall
@@ -5,8 +9,12 @@ apt update && apt upgrade -y # get updates, install nfs, and apply
 apt install nfs-common -y # install nfs
 apt autoremove -y # clean up
 
+# for the server node
 # install rke2
 curl -sfL https://get.rke2.io | sh - # install rke2
+mkdir -p /etc/rancher/rke2/ # create directory
+touch /etc/rancher/rke2/config.yaml # create file
+echo "enable-servicelb: true" >> /etc/rancher/rke2/config.yaml # write kubeconfig mode
 systemctl enable rke2-server.service # enable rke2
 systemctl start rke2-server.service # start rke2
 
@@ -18,6 +26,18 @@ export KUBECONFIG=/etc/rancher/rke2/rke2.yaml # for current session
 echo "KUBECONFIG='/etc/rancher/rke2/rke2.yaml'" >> ~/.bashrc # for future sessions
 source ~/.bashrc
 
+# for the agent node
+mkdir -p /etc/rancher/rke2/
+touch /etc/rancher/rke2/config.yaml
+echo "server: https://192.168.0.10:9345
+token: K10daafaec98e8330e638e60386a7624be441c481396558be0bd71fa68c28df12af::server:64d1e7cf7c8ed7135b38ca5e33f8d270" >> /etc/rancher/rke2/config.yaml
+
+curl -sfL https://get.rke2.io | INSTALL_RKE2_TYPE="agent" sh -
+systemctl enable rke2-agent.service
+systemctl start rke2-agent.service
+
+
+#in the server node
 # install helm
 curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
 
@@ -30,14 +50,11 @@ helm repo add rancher-latest https://releases.rancher.com/server-charts/latest
 helm repo add jetstack https://charts.jetstack.io
 
 # install cert-manager
-helm install cert-manager jetstack/cert-manager \ 
-  --namespace cert-manager \
-  --create-namespace \
-  --set installCRDs=true
+helm upgrade --install --namespace cert-manager --create-namespace cert-manager jetstack/cert-manager --set crds.enable=true
 
 # install rancher
-helm install rancher rancher-latest/rancher \
-  --namespace cattle-system \
-  --create-namespace \
-  --set hostname=rancher.mydomain.com \
-  --set bootstrapPassword=admin
+helm install rancher rancher-latest/rancher --namespace cattle-system --create-namespace --set hostname=rancher.mangotecnologia.com.br --set bootstrapPassword=admin 
+
+# admin
+# zUoL2V$WFhIp$sx
+# kubectl -n cattle-system exec $(kubectl -n cattle-system get pods -l app=rancher --no-headers | head -1 | awk '{ print $1 }') -c rancher -- reset-password
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # rancher-localhost
+
+[Parte 1](https://medium.com/@luylucas10/rancher-for-devs-83854a6f08e7)
+
+[Parte 2](https://medium.com/@luylucas10/rancher-for-devs-part-2-0f9fe4596eaa)
